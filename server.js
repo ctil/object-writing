e79e3e6de55b873a05381a457dd20fbe9be0d80e
Colin Tilleman <cjtilleman@gmail.com>
2014-12-09 21:21:07 -0800

    Import morgan.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var express        = require('express');
-var app            = express();
-var mongoose       = require('mongoose');
-var bodyParser     = require('body-parser');
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+// Log requests to console
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 var port = process.env.PORT || 8080;
@@ -11,6 +13,8 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(express.static(__dirname + '/public'));
+// Log every request to the console
+app.use(morgan('dev'));
 
 // routes
 require('./app/routes')(app);
