e5a93dbe5a5692ac03f88876ab080324343f487a
Colin Tilleman <cjtilleman@gmail.com>
2015-01-18 19:32:37 -0800

    Added timer.

--- public/js/controllers/SessionCtrl.js
+++ public/js/controllers/SessionCtrl.js
@@ -1,3 +1,21 @@
-angular.module('SessionCtrl', []).controller('SessionController', ['$scope', '$http', 'settings', function($scope, $http, settings) {
-    $scope.settings = settings;
+angular.module('SessionCtrl', []).controller('SessionController', ['$scope', '$http', 'settings', '$interval', function($scope, $http, settings, $interval) {
+    $scope.word = settings.word;
+    var timerSeconds = settings.timerMinutes * 60; 
+    $scope.secondsLeft = timerSeconds;
+    document.getElementById('writing-area').focus();
+
+    function updateTimer() {
+	$scope.secondsLeft -= 1;
+	$scope.percentage = $scope.secondsLeft/(timerSeconds) * 100;
+	if ( $scope.secondsLeft <= 0) {
+	    $interval.cancel(stop);
+	    alert('Time is up!');
+	}
+
+    };
+    var stop = $interval(updateTimer, 1000);
+
+    $scope.$on('$destroy', function() {
+        $interval.cancel(stop);
+    });
 }]);
